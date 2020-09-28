de4b2904e750ab004757e4e4901d07b59fae0d44
https://github.com/WillXXX/AtmConsult.git <will.marcx@gmail.com>
2020-09-28 17:12:54 -0300

    *buttons

--- app/src/main/java/com/example/atmconsult/MainActivity.kt
+++ app/src/main/java/com/example/atmconsult/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.atmconsult
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_clientes.setOnClickListener {
+            val intent = Intent(this,DetalheClienteActivity::class.java)
+            startActivity(intent)
+        }
+
+        button_empresa.setOnClickListener {
+            val intent = Intent(this,DetalheEmpresa::class.java)
+            startActivity(intent)
+        }
     }
 }
